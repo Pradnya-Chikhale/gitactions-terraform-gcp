3aa23d96bfd105b519ca84078bd5f22948b69b66
Pradnya Chikhale <pradnyach27@gmail.com>
2024-01-06 19:25:01 +0530

    commiting terraform changes

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket"
-  project                  = "My First Project"
+  name                     = "githubdemo-bucket"
+  project                  = "vital-petal-409810"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
