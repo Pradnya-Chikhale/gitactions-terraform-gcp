8c25e220112e53e4918c91aa2d366ad8bc672536
Pradnya Chikhale <pradnyach27@gmail.com>
2024-01-06 23:42:50 +0530

    new python app

--- main.tf
+++ main.tf
@@ -1,7 +1,24 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "githubdemo-bucket"
-  project                  = "vital-petal-409810"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+#resource "google_storage_bucket" "my-bucket" {
+#  name                     = "githubdemo-bucket"
+# project                  = "vital-petal-409810"
+#  location                 = "US"
+#  force_destroy            = true
+#  public_access_prevention = "enforced"
+#}
+
+provider "google" {
+  credentials = file("terraform.json")
+  project     = "vital-petal-409810"
+  region      = "us-central1"
+}
+resource "google_cloud_run_service" "django_app" {
+  name     = "django-app"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "gcr.io/vital-petal-409810/gitactions-terraform-app"
+      }
+    }
+  }
 }
